95c5daac9806514b0603577e87a4cd2c81f91585
Liam Woods <143724842+Liam-Woods-8@users.noreply.github.com>
2026-02-23 02:29:11 +0000

    hw5 setup

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ hw1_page = st.Page("HW/HW1.py", title="HW 1")
 hw2_page = st.Page("HW/HW2.py", title="HW 2")
 hw3_page = st.Page("HW/HW3.py", title="HW 3")
 hw4_page = st.Page("HW/HW4.py", title="HW 4")
+hw5_page = st.Page("HW/HW5.py", title="HW 5")
 
-pg = st.navigation([hw4_page, hw3_page, hw2_page, hw1_page])  
+pg = st.navigation([hw5_page, hw4_page, hw3_page, hw2_page, hw1_page])  
 pg.run()
